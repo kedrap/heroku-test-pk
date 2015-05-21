9c335dd1c5a446dfb7969cd1ec7328ae92ab21f0
Piotr Kedra <piotr.kedra@schibsted.pl>
2015-05-21 08:34:45 +0200

    Add calls to mark transactions

--- public_html/command.php
+++ public_html/command.php
@@ -3,7 +3,11 @@
 $who = $argv[1];
 echo 'Hello ' . $who . "\n";
 
-for ($nr = 0; $nr < 3; $nr++) {
+for ($nr = 0; $nr < 150; $nr++) {
+    newrelic_start_transaction('PHP Application on Heroku');
+
     sleep(6);
     echo $nr . "\n";
+
+    newrelic_end_transaction('PHP Application on Heroku');
 }
